4f459aa6a061bba7537c565344bb8af31907855b
adiiityaaa <adityak3653@gmail.com>
2023-04-01 23:42:25 +0530

    Program File Codes

---
+++ Practical Files/Sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout<<"Size of Int is "<<sizeof(int)<<" byte(s)."<<endl;
+ cout<<"Size of Short Int is "<<sizeof(short int)<<" byte(s)."<<endl;
+ cout<<"Size of Long Int is "<<sizeof(long int)<<" byte(s)."<<endl;
+ cout<<"Size of Char is "<<sizeof(char)<<" byte(s)."<<endl;
+ cout<<"Size of Float is "<<sizeof(float)<<" byte(s)."<<endl;
+ cout<<"Size of Double is "<<sizeof(double)<<" byte(s)."<<endl;
+ cout<<"Size of Boolean is "<<sizeof(bool)<<" byte(s)."<<endl;
+}
